dbedd17fa49ed83380c70196b9516357d0f2104f
Ian Page Hands <ihands@redhat.com>
2014-08-05 15:20:48 -0400

    updating

--- .emacs.d/elpa/js3-mode-20140618.1914/js3-mode-autoloads.el
+++
@@ -1,45 +0,0 @@
-;;; js3-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (js3-mode) "js3" "js3.el" (21416 22894 952397 878000))
-;;; Generated autoloads from js3.el
- (add-to-list 'auto-mode-alist '("\\.js$" . js3-mode))
-
-(autoload 'js3-mode "js3" "\
-Major mode for editing JavaScript code.
-
-\\{js3-mode-map}
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads (js3-mode) "js3-mode" "js3-mode.el" (21416 22894
-;;;;;;  971397 734000))
-;;; Generated autoloads from js3-mode.el
- (add-to-list 'auto-mode-alist '("\\.js$" . js3-mode))
-
-(autoload 'js3-mode "js3-mode" "\
-Major mode for editing JavaScript code.
-
-\\{js3-mode-map}
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("js3-mode-pkg.el") (21416 22894 986920
-;;;;;;  591000))
-
-;;;***
-
-(provide 'js3-mode-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; js3-mode-autoloads.el ends here
--- .emacs.d/elpa/s-20140620.1657/s-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; s-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("s-pkg.el" "s.el") (21416 22893 741076
-;;;;;;  139000))
-
-;;;***
-
-(provide 's-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; s-autoloads.el ends here
