3637b0030a8e15ff22b1b1571806b306fff24aac
Ian Page Hands <ihands@redhat.com>
2014-10-07 18:43:18 -0400

    updated

---
+++ .emacs.d/elpa/deferred-20140816.2205/deferred-autoloads.el
@@ -0,0 +1,18 @@
+;;; deferred-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("deferred-pkg.el" "deferred.el") (21492
+;;;;;;  56194 156798 996000))
+
+;;;***
+
+(provide 'deferred-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; deferred-autoloads.el ends here
