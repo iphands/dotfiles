6897c34323bcff3b75eb26addbbd556539109ddd
Ian Page Hands <ihands@redhat.com>
2015-07-30 04:51:41 -0400

    update and added dockerfilemode

---
+++ .emacs.d/elpa/drupal-mode-20150709.1219/drupal-mode-pkg.el
@@ -0,0 +1,7 @@
+(define-package "drupal-mode" "20150709.1219" "Advanced minor mode for Drupal development"
+  '((php-mode "1.5.0"))
+  :url "https://github.com/arnested/drupal-mode" :keywords
+  '("programming" "php" "drupal"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
