a65affcf0d9e92e8fd18705be9f76efc92ab7c91
Ian Page Hands <iphands@gmail.com>
2018-12-14 18:11:38 -0500

    update

---
+++ .emacs.d/elpa/git-commit-20181206.2347/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-commit" "20181206.2347" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (with-editor "20181103")) :commit "9d9290bc27df884a4e3334da3b45bb108dd7d22f" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
