03345dea7b938374880e3be7762a010ff7d4e2c4
Ian Page Hands <iphands@gmail.com>
2021-03-12 20:48:05 -0500

    bump update

---
+++ .emacs.d/elpa/cmake-mode-20210104.1831/cmake-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/iphands/.emacs.d/elpa/cmake-mode-20210104.1831/cmake-mode.el  -*- no-byte-compile: t -*-
+(define-package "cmake-mode" "20210104.1831" "major-mode for editing CMake sources" '((emacs "24.1")) :commit "b5f60c54d02ba9df7f2ba67200efb97446e0417a")
