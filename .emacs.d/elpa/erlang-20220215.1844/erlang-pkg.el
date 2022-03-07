95e105e0b379891935ec2c340c0f21a1abe46636
Ian Page Hands <iphands@gmail.com>
2022-03-07 08:01:00 -0500

    update bump

---
+++ .emacs.d/elpa/erlang-20220215.1844/erlang-pkg.el
@@ -0,0 +1,11 @@
+(define-package "erlang" "20220215.1844" "Erlang major mode"
+  '((emacs "24.1"))
+  :commit "dc332d2102bf7ea5f8d60775d863150b40ca4cff" :authors
+  '(("Anders Lindgren"))
+  :maintainer
+  '("Anders Lindgren")
+  :keywords
+  '("erlang" "languages" "processes"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ .emacs.d/elpa/helm-20220305.1000/helm-pkg.el
@@ -0,0 +1,11 @@
+(define-package "helm" "20220305.1000" "Helm is an Emacs incremental and narrowing framework"
+  '((helm-core "3.8.4")
+    (popup "0.5.3"))
+  :commit "f2e6d91c943d7ba0c69fbfc832a9e88fecd124b9" :authors
+  '(("Thierry Volpiatto" . "thierry.volpiatto@gmail.com"))
+  :maintainer
+  '("Thierry Volpiatto" . "thierry.volpiatto@gmail.com")
+  :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
