2c3ffd1c7b7bc464a81e5908582eb6926465c625
Ian Page Hands <iphands@gmail.com>
2023-01-01 21:59:33 -0800

    bump update packages

--- .emacs.d/elpa/protobuf-mode-20220303.1716/protobuf-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from protobuf-mode.el  -*- no-byte-compile: t -*-
-(define-package "protobuf-mode" "20220303.1716" "major mode for editing protocol buffers." 'nil :commit "349d74d92e8268c41fbb726b0a8292d588c63e20" :authors '(("Alexandre Vassalotti" . "alexandre@peadrop.com")) :maintainer '("Alexandre Vassalotti" . "alexandre@peadrop.com") :keywords '("google" "protobuf" "languages"))
