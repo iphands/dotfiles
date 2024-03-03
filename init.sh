9eb9a44166428827c75b17e250d29dd216394a0c
Ian Page Hands <iphands@gmail.com>
2024-03-03 08:11:59 -0800

    install host specific

--- init.sh
+++ init.sh
@@ -3,6 +3,38 @@
 current="$HOME/.dotfiles_backup/`date +%Y%m%d%H%M%S`"
 mkdir -p "$current"
 
+install_scripts() {
+  p=$1
+  echo
+  echo "## $p scripts"
+  mkdir -p ~/$p
+  pushd ./$p >/dev/null
+  for var in *
+  do
+    echo "-- installing ~/$p/$var"
+    ln -sf $PWD/$var ~/$p/
+  done
+  popd >/dev/null
+}
+
+install_host() {
+  git_target=$1
+  target=$2
+  if [[ "$HOSTNAME" =~ "PC2K41CN" ]]
+  then
+    host=handslap
+  fi
+
+  pushd host/$host/$git_target >/dev/null
+  mkdir -p ~/$target
+  for var in *
+  do
+    echo "-- installing ~/$target/$var"
+    ln -sf $PWD/$var ~/$target/$var
+  done
+  popd >/dev/null
+}
+
 echo "## Dotfiles"
 for var in ".Xdefaults"\
   ".alacritty.yml" \
@@ -24,29 +56,18 @@ do
   fi
 done
 
-install() {
-  p=$1
-  echo
-  echo "## $p scripts"
-  mkdir -p ~/$p
-  pushd ./$p >/dev/null
-  for var in *
-  do
-    echo "-- installing ~/$p/$var"
-    ln -sf $PWD/$var ~/$p/
-  done
-  popd >/dev/null
-}
-
-install bin
+install_scripts bin
 
 if [ "$EUID" -eq 0 ]
 then
-  install sbin
+  install_scripts sbin
 fi
 
+echo
+echo "## Host specific"
+install_host fluxbox .fluxbox
 
-if [ ! -f /etc/profile.d/git-prompt.sh ]
-then
-  echo "# ln -s /usr/share/git-core/contrib/completion/git-prompt.sh  /etc/profile.d/"
-fi
+# if [ ! -f /etc/profile.d/git-prompt.sh ]
+# then
+#   echo "# ln -s /usr/share/git-core/contrib/completion/git-prompt.sh  /etc/profile.d/"
+# fi
