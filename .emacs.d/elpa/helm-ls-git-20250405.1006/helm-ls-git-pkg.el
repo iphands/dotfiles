458755786c30862e1443a4423d8d6924eb90ed9a
Ian Page Hands <iphands@gmail.com>
2025-05-28 11:09:17 -0700

    updoot

--- .emacs.d/elpa/helm-ls-git-20250405.1006/helm-ls-git-pkg.el
+++
@@ -1,9 +0,0 @@
-;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "helm-ls-git" "20250405.1006"
-  "The git project manager for helm."
-  '((helm  "3.9.5")
-    (emacs "25.3"))
-  :url "https://github.com/emacs-helm/helm-ls-git"
-  :commit "05bbc079af48721d1f48c1f58186a84625ddf984"
-  :revdesc "05bbc079af48"
-  :keywords '("helm" "convenience" "vc" "files" "buffers" "completion" "diff" "log" "git"))
