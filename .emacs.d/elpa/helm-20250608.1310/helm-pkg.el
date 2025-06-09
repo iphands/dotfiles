bd34ee74980d80b86ad527aeec7b96c9f9b6ce10
Ian Page Hands <iphands@gmail.com>
2025-06-09 11:17:08 -0700

    update emacs packages

---
+++ .emacs.d/elpa/helm-20250608.1310/helm-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "helm" "20250608.1310"
+  "Helm is an Emacs incremental and narrowing framework."
+  '((helm-core "4.0.3")
+    (wfnames   "1.2"))
+  :url "https://emacs-helm.github.io/helm/"
+  :commit "b15dcb7d8098783ed861d44c525b4c29e50d06ab"
+  :revdesc "b15dcb7d8098"
+  :keywords '("helm" "convenience" "files" "buffers" "grep" "completion" "lisp" "matching" "tools" "help")
+  :authors '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainers '(("Thierry Volpiatto" . "thievol@posteo.net")))
---
+++ .emacs.d/elpa/lsp-mode-20250609.1505/lsp-mode-pkg.el
@@ -0,0 +1,15 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "lsp-mode" "20250609.1505"
+  "LSP mode."
+  '((emacs         "28.1")
+    (dash          "2.18.0")
+    (f             "0.20.0")
+    (ht            "2.3")
+    (spinner       "1.7.3")
+    (markdown-mode "2.3")
+    (lv            "0")
+    (eldoc         "1.11"))
+  :url "https://github.com/emacs-lsp/lsp-mode"
+  :commit "1d5f7698b8884cafa066c9812805d43cffe71cbc"
+  :revdesc "1d5f7698b888"
+  :keywords '("languages"))
